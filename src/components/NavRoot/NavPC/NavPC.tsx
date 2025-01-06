7f17e76f223fe5d1d35d5cdc3a4f21c3b9a8de81
Ryan DECIAN <ryan.decian.propgmail.com>
2025-01-06 12:47:25 +0100

    Copie du projet bille pour un test de déploiement

--- src/components/NavRoot/NavPC/NavPC.tsx
+++ src/components/NavRoot/NavPC/NavPC.tsx
@@ -1,36 +1,7 @@
 import { Link } from "react-router-dom"
 import "./NavPC.css"
-import { useState } from "react";
-import { useEffect } from "react";
 
 function NavPC() {
-     const [dimensions, setDimensions] = useState({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      
-        useEffect(() => {
-          const handleResize = () => {
-            setDimensions({
-              width: window.innerWidth,
-              height: window.innerHeight,
-            });
-          };
-      
-          window.addEventListener("resize", handleResize);
-      
-          // Nettoyage pour éviter les fuites de mémoire
-          return () => {
-            window.removeEventListener("resize", handleResize);
-          };
-        }, []);
-
-        const [screen, setScreen] = useState(dimensions.width * dimensions.height / 11203)
-            useEffect(() => {
-                setScreen(dimensions.width * dimensions.height / 11203);
-              }, [dimensions]);
-      
-      
 
     return (
         <nav className="NavPC">
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import BilleM7 from './pages/BilleM7/BilleM7.tsx'
+import BilleM5 from './pages/BilleM5/BilleM5.tsx'
+import BilleM3 from './pages/BilleM3/BilleM3.tsx'
+import BilleM2 from './pages/BilleM2/BilleM2.tsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+      path: "/",
+      element: <BilleM7 />
+      },
+      {
+      path: "/billeM7",
+      element: <BilleM7 />
+      },
+      {
+      path: "/billeM5",
+      element: <BilleM5 />
+      },
+      {
+      path: "/billeM3",
+      element: <BilleM3 />
+      },
+      {
+      path: "/billeM2",
+      element: <BilleM2 />
+      },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
 )
